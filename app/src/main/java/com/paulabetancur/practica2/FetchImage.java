2b5900d9a4bcc33c7f14e0ab6466bc8ad48e6216
paulab10 <paulab10>
2017-10-01 09:40:56 -0500

    V4

---
+++ app/src/main/java/com/paulabetancur/practica2/FetchImage.java
@@ -0,0 +1,54 @@
+package com.paulabetancur.practica2;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import java.net.URL;
+
+/**
+ * Created by HOME-I on 2/06/2017.
+ */
+
+public class FetchImage extends AsyncTask<String, Void, Bitmap> {
+
+    public interface AsyncResponse{
+        void processFinish(Bitmap bitmap);
+    }
+
+    private Context context;
+    private final AsyncResponse delegate;
+
+    public FetchImage(Context context, AsyncResponse listener){
+        this.context = context;
+        this.delegate = listener;
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... objects) {
+        Bitmap bitmap = null;
+        try {
+            URL imageURL = new URL(objects[0]);
+            bitmap = BitmapFactory.decodeStream(imageURL.openConnection().getInputStream());
+        }catch (Exception e){
+            Log.d("URL Exception", e.toString());
+        }finally {
+            // Return image
+            return bitmap;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        super.onPostExecute(bitmap);
+        if (bitmap == null){
+            delegate.processFinish(null);
+        }
+        else {
+            if(this.delegate != null)
+                delegate.processFinish(bitmap);
+        }
+    }
+}
